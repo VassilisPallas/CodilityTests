6f12093fc69c1fca76d96e1064aee60bcadc2263
VassilisPallas <vspallas@gmail.com>
2017-03-05 02:00:13 +0200

    lesson 2 a

--- src/Lesson2/OddOccurrencesInArray.java
+++ src/Lesson2/OddOccurrencesInArray.java
@@ -8,6 +8,8 @@ public class OddOccurrencesInArray {
     public int solution(int[] A) {
         int result = A[0];
         for (int i = 1; i < A.length; i++) {
+            // XOR flips certain bits. By applying XOR with the same numbers again with the same integer later on,
+            // we flipping them back. The result is the number that never canceled itself out.
             result ^= A[i];
         }
 
